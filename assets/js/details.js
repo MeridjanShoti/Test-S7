6c4ac2a50b5d6cf6851e641f600021a2221e3015
MeridjanShoti <meridjanshoti@gmail.com>
2025-01-24 20:31:45 +0100

    a

--- assets/js/details.js
+++ assets/js/details.js
@@ -17,18 +17,20 @@ fetch("https://striveschool-api.herokuapp.com/api/product/" + productId, {
   })
   .then((product) => {
     const title = document.createElement("h1");
-    title.classList.add("text-center");
+    title.classList.add("text-center", "mt-5");
     title.innerText = product.name;
     const img = document.createElement("img");
     img.src = product.imageUrl;
-    img.classList.add("img-fluid");
+    img.classList.add("w-50", "mx-auto");
     img.alt = product.name;
     const brand = document.createElement("h3");
     brand.classList.add("text-center");
     brand.innerText = product.brand;
     const description = document.createElement("p");
+    description.classList.add("border-bottom", "border-secondary");
     description.innerText = product.description;
     const price = document.createElement("p");
+    price.classList.add("border-bottom", "border-secondary", "text-center", "fw-bold");
     price.innerText = "prezzo: " + product.price + "€";
 
     details.appendChild(title);
