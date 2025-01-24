b3915abfc1206ceb75cf7ae31f79b00e2c7feb38
MeridjanShoti <meridjanshoti@gmail.com>
2025-01-24 12:52:39 +0100

    a

--- assets/js/backoffice.js
+++ assets/js/backoffice.js
@@ -1,16 +1,44 @@
 const backofficeForm = document.getElementById("backofficeForm");
 const params = new URLSearchParams(window.location.search);
 const productId = params.get("productId");
-const URL = productId? "https://striveschool-api.herokuapp.com/api/product/" + productId
-fetch("https://striveschool-api.herokuapp.com/api/product/", {
-    method: "GET",
+const productName = document.getElementById("productName");
+const productDescription = document.getElementById("productDescription");
+const productBrand = document.getElementById("productName");
+const productImg = document.getElementById("productImg");
+const productPrice = document.getElementById("productPrice");
+
+const URL = productId
+  ? "https://striveschool-api.herokuapp.com/api/product/" + productId
+  : "https://striveschool-api.herokuapp.com/api/product/";
+const insertProduct = document.getElementById("insertProduct");
+if (productId) {
+  fetch(URL, {
+    method: "get",
     headers: {
-    Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzkzNmU2ZGI3NDcwMTAwMTU4YjJiOTgiLCJpYXQiOjE3Mzc3MTUzMDksImV4cCI6MTczODkyNDkwOX0.WTRnC2bHipbz_gG9ajIUdn6kzhAZhv1L3u1JzIUvL2o"
-    }
-    }).then(response => {
-        if (response.ok){
-            return response.json()
-        }
-    }).then(product => {
-        console.log(product)
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzkzNmU2ZGI3NDcwMTAwMTU4YjJiOTgiLCJpYXQiOjE3Mzc3MTUzMDksImV4cCI6MTczODkyNDkwOX0.WTRnC2bHipbz_gG9ajIUdn6kzhAZhv1L3u1JzIUvL2o",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
     })
+    .then((product) => {
+      backofficeForm.elements.name.value = product.name;
+      console.log(backofficeForm);
+    });
+}
+fetch(URL, {
+  method: productId ? "PUT" : "POST",
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzkzNmU2ZGI3NDcwMTAwMTU4YjJiOTgiLCJpYXQiOjE3Mzc3MTUzMDksImV4cCI6MTczODkyNDkwOX0.WTRnC2bHipbz_gG9ajIUdn6kzhAZhv1L3u1JzIUvL2o",
+  },
+})
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+  })
+  .then((product) => {});
