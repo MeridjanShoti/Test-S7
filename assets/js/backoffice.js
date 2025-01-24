d1b0c95cebe30cf9c09f0297aaf75eed2df9f176
MeridjanShoti <meridjanshoti@gmail.com>
2025-01-24 14:12:14 +0100

    a

--- assets/js/backoffice.js
+++ assets/js/backoffice.js
@@ -23,35 +23,37 @@ if (productId) {
       backofficeForm.elements.name.value = product.name;
       console.log(backofficeForm);
     });
-
-  backofficeForm.onsubmit = (e) => {
-    e.preventDefault();
-    const newProduct = {
-      productName: document.getElementById("productName").value,
-      productDescription: document.getElementById("productDescription").value,
-      productBrand: document.getElementById("productName").value,
-      productImg: document.getElementById("productImg").value,
-      productPrice: document.getElementById("productPrice").value,
-    };
-    fetch(URL, {
-      method: productId ? "PUT" : "POST",
-      body: JSON.stringify(newProduct),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzkzNmU2ZGI3NDcwMTAwMTU4YjJiOTgiLCJpYXQiOjE3Mzc3MTUzMDksImV4cCI6MTczODkyNDkwOX0.WTRnC2bHipbz_gG9ajIUdn6kzhAZhv1L3u1JzIUvL2o",
-      },
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error();
-        }
-      })
-      .then((product) => {
-        console.log(product);
-      })
-      .catch((error) => console.log(error));
-  };
 }
+console.log(backofficeForm);
+backofficeForm.onsubmit = function (e) {
+  e.preventDefault();
+  alert("submit corretto");
+  const newProduct = {
+    name: document.getElementById("productName").value,
+    description: document.getElementById("productDescription").value,
+    brand: document.getElementById("productBrand").value,
+    imageUrl: document.getElementById("productImg").value,
+    price: document.getElementById("productPrice").value,
+  };
+  console.log(newProduct);
+  fetch(URL, {
+    method: productId ? "PUT" : "POST",
+    body: JSON.stringify(newProduct),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzkzNmU2ZGI3NDcwMTAwMTU4YjJiOTgiLCJpYXQiOjE3Mzc3MTUzMDksImV4cCI6MTczODkyNDkwOX0.WTRnC2bHipbz_gG9ajIUdn6kzhAZhv1L3u1JzIUvL2o",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error();
+      }
+    })
+    .then((product) => {
+      console.log(product);
+    })
+    .catch((error) => console.log(error));
+};
